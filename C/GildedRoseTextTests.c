71af798b9c6c2d864606d4a3ac5143055183e401
Joseph Bennett <jbennett@jbennettconsulting.com>
2021-02-26 23:14:58 -0500

    changes

--- C/GildedRoseTextTests.c
+++ C/GildedRoseTextTests.c
@@ -7,42 +7,30 @@ print_item(Item *item)
     return printf("%s, %d, %d\n", item->name, item->sellIn, item->quality);
 }
 
-int
-print_int(int *integer)
-{
-    return printf("integer %d\n", *integer);
-}
-
 int main()
 {
-    int integer_list[9];
-    for(int i=0;i<9;i++)
-        integer_list[i] = 10-i;
-    for(int i=0;i<9;i++)
-        print_int(integer_list + i);
-//    Item items[9];
-//    int last = 0;
-//    int day;
-//    int index;
-    
-//    init_item(items + last++, "Aged Brie", 2, 0);
-//    printf("Last %d\n", last);
-//    update_quality(items, 1);
-//    print_item(items + 0);
-//    init_item(items + last++, "+5 Dexterity Vest", 10, 20);
-//    init_item(items + last++, "Aged Brie", 2, 0);
-//    init_item(items + last++, "Elixir of the Mongoose", 5, 7);
-//    init_item(items + last++, "Sulfuras, Hand of Ragnaros", 0, 80);
-//    init_item(items + last++, "Sulfuras, Hand of Ragnaros", -1, 80);
-//    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 15, 20);
-//    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 10, 49);
-//    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 5, 49);
+    Item items[9];
+    int last = 0;
+    int day;
+    int index;
+  
+    init_item(items + last++, "Aged Brie", 2, 0);
+    printf("Last %d\n", last);
+    update_quality(items, 1);
+    print_item(items + 0);
+    init_item(items + last++, "+5 Dexterity Vest", 10, 20);
+    init_item(items + last++, "Aged Brie", 2, 0);
+    init_item(items + last++, "Elixir of the Mongoose", 5, 7);
+    init_item(items + last++, "Sulfuras, Hand of Ragnaros", 0, 80);
+    init_item(items + last++, "Sulfuras, Hand of Ragnaros", -1, 80);
+    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 15, 20);
+    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 10, 49);
+    init_item(items + last++, "Backstage passes to a TAFKAL80ETC concert", 5, 49);
     // this Conjured item doesn't yet work properly
-//    init_item(items + last++, "Conjured Mana Cake", 3, 6);
+    init_item(items + last++, "Conjured Mana Cake", 3, 6);
     
 //    puts("OMGHAI!");
-/*
-    for (day = 0; day <= 30; day++)
+    for (day = 0; day <= 10; day++)
     {
         printf("-------- day %d --------\n", day);
         printf("name, sellIn, quality\n");
@@ -55,6 +43,5 @@ int main()
         update_quality(items, last);
     }
     printf("FOOBAR\n");
-*/
     return 0;
 }
